8b7b1005684beaa786f6afb25a0f78a5413fef3f
Andrey Akimov <a.a.m.incorp@yandex.ru>
2016-10-18 00:29:20 +0300

    last changes

--- wss.js
+++ wss.js
@@ -1,7 +1,8 @@
 "use strict";
 const WS = require('ws');
 
-const HOST = 'wss://iotmmsp1942206778trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/ws/data/50f2e452-c95d-4168-920c-e07f7c8ef962';
+
+const HOST = 'wss://iotmmsp1942206778trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/ws/data/e527ea4a-6a43-4ff6-8296-4ac89fd0f8c7';
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -11,11 +12,12 @@ function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
+
 class Sensor {
 
-    constructor(id_, class_, io_port_) {
+    constructor(id_, io_port_) {
         this.io_port = io_port_;
-        this.msg = {sensor_id: id_, sensor_class: class_, sensor_value: 0, timestamp: "0"};
+        this.msg = {sensor_id: id_, sensor_value: 0};
     }
 
     get id() {
@@ -29,13 +31,14 @@ class Sensor {
 
     readInput() {
         this.msg.sensor_value = getRandomArbitrary(this.id * 10 * (1 - 0.05), this.id * 10 * (1 + 0.05));
+        // this.msg.sensor_value = getLight();
     }
 
     getMessage() {
 
         var sap_msg_struct = {
             "mode":"async",
-            "messageType":"bfdb9f931da6f202de05",
+            "messageType":"b17a4204fca0e5bf7d52",
             "messages": [this.msg]
         };
 
@@ -49,7 +52,7 @@ class Sensor {
 
 const OPTIONS = {
     headers: {
-        Authorization: 'Bearer 4c721e1eeb7e3be80d9a96c609a42fa'
+        Authorization: 'Bearer 74505559a34962d0781bd448fa7c22d'
     }
 }
 
@@ -57,8 +60,7 @@ const OPTIONS = {
 
 
 var sensors = [
-    new Sensor('1', 'WATER', 1),
-    new Sensor('2', 'LIGHT', 2)
+    new Sensor(1, 1)
 ];
 
 
@@ -79,13 +81,13 @@ function setupWebSocket() {
                 ws.send( sensor.getMessage() );
             });
 
-        }, 1000);
+        }, 4000);
     };
 
     ws.onerror = (err) => { console.log(err+'') };
 
     ws.onmessage = (msg_) => {
-        console.log("<<:", msg_.data);
+        console.log("<<", msg_.data);
     };
 
     ws.onclose = () => {
